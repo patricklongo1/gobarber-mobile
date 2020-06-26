66e22cbf09bfcb817177596b4ac612e11315b1c2
patricklongo1 <ck.longo@bol.com.br>
2020-06-26 11:39:31 -0300

    api connected with localhost / adb reverse

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -70,7 +70,7 @@ const Input: React.RefForwardingComponent<InputVRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container isFocused={isFocused}>
+    <Container isFocused={isFocused} isErrored={!!error}>
       <Icon
         name={icon}
         size={20}
